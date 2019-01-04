b2aebe0966f490479bca2a73e8c136cbb7edb0ca
hyyerr0 <43856365+hyyerr0@users.noreply.github.com>
2019-01-04 18:06:58 +0300

    edited
    etited

--- views/backend/default/index.php
+++ views/backend/default/index.php
@@ -6,7 +6,7 @@
 
         <p class="lead">Butce Modülüne Hoşgeldiniz</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://portal.kouosl/admin/butcee/create">Modül Oluştur</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://portal.kouosl/admin/butcee/create">Bütçe Girişi</a></p>
         <p><a class="btn btn-lg btn-danger" href="http://portal.kouosl/admin/butcee/index">Şirketleri Görüntüle</a></p>
     </div>
 
